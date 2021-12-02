a8674f5a2006fa47dbf4146f353c1a3bbaf69d57
Wilmar Stiven Valencia Cardona <wilmar2545@gmail.com>
2021-12-02 09:08:03 -0500

    modify router.ex

--- lib/phoenix_path_web/router.ex
+++ lib/phoenix_path_web/router.ex
@@ -2,24 +2,24 @@ defmodule PhoenixPathWeb.Router do
   use PhoenixPathWeb, :router
 
   pipeline :browser do
-    plug(:accepts, ["html"])
-    plug(:fetch_session)
-    plug(:fetch_live_flash)
-    plug(:put_root_layout, {PhoenixPathWeb.LayoutView, :root})
-    plug(:protect_from_forgery)
-    plug(:put_secure_browser_headers)
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_live_flash
+    plug :put_root_layout, {PhoenixPathWeb.LayoutView, :root}
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
   end
 
   pipeline :api do
-    plug(:accepts, ["json"])
+    plug :accepts, ["json"]
   end
 
   scope "/", PhoenixPathWeb do
-    pipe_through(:browser)
+    pipe_through :browser
 
-    get("/", PageController, :index)
-    get("/hello", HelloController, :index)
-    get("/hello/:messenger", HelloController, :show)
+    get "/", PageController, :index
+    get "/hello", HelloController, :index
+    get "/hello/:messenger", HelloController, :show
   end
 
   # Other scopes may use custom stacks.
@@ -38,8 +38,8 @@ defmodule PhoenixPathWeb.Router do
     import Phoenix.LiveDashboard.Router
 
     scope "/" do
-      pipe_through(:browser)
-      live_dashboard("/dashboard", metrics: PhoenixPathWeb.Telemetry)
+      pipe_through :browser
+      live_dashboard "/dashboard", metrics: PhoenixPathWeb.Telemetry
     end
   end
 
@@ -49,9 +49,9 @@ defmodule PhoenixPathWeb.Router do
   # node running the Phoenix server.
   if Mix.env() == :dev do
     scope "/dev" do
-      pipe_through(:browser)
+      pipe_through :browser
 
-      forward("/mailbox", Plug.Swoosh.MailboxPreview)
+      forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
   end
 end
