de467da4b9274fffae78c64af8a416c5c120508c
Wilmar Stiven Valencia Cardona <wilmar2545@gmail.com>
2021-12-04 10:25:09 -0500

    Create a chatroom using Channels

--- assets/js/user_socket.js
+++ assets/js/user_socket.js
@@ -58,6 +58,23 @@ socket.connect()
 // subtopic is its id - in this case 42:
 let channel = socket.channel("room:lobby", {})
 
+let chatInput = document.querySelector("#chat-input")
+let messagesContainer = document.querySelector("#messages")
+
+chatInput.addEventListener("keypress", event => {
+  if (event.key === 'Enter') {
+    channel.push("new_msg", { body: chatInput.value })
+    chatInput.value = ""
+  }
+})
+
+channel.on("new_msg", payload => {
+  console.log(payload.body)
+  let messageItem = document.createElement("p")
+  messageItem.innerText = `[${Date()}] ${payload.body}`
+  messagesContainer.appendChild(messageItem)
+})
+
 channel.join()
   .receive("ok", resp => { console.log("Joined successfully", resp) })
   .receive("error", resp => { console.log("Unable to join", resp) })
