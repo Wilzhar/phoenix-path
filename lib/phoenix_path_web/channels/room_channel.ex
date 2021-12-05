9d202c604147253e9ffa07dcb801a15c955e2f30
Wilmar Stiven Valencia Cardona <wilmar2545@gmail.com>
2021-12-05 17:41:36 -0500

    Create presence to count users in live

--- lib/phoenix_path_web/channels/room_channel.ex
+++ lib/phoenix_path_web/channels/room_channel.ex
@@ -1,5 +1,11 @@
 defmodule PhoenixPathWeb.RoomChannel do
   use Phoenix.Channel
+  alias PhoenixPathWeb.Presence
+
+  def join("room:lobby", %{"name" => name}, socket) do
+    send(self(), :after_join)
+    {:ok, assign(socket, :name, name)}
+  end
 
   def join("room:lobby", _message, socket) do
     {:ok, socket}
@@ -7,6 +13,16 @@ defmodule PhoenixPathWeb.RoomChannel do
 
   def join("room:" <> _private_room_id, _params, _socket) do
     {:error, %{reason: "unauthorized"}}
+  end
+
+  def handle_info(:after_join, socket) do
+    {:ok, _} =
+      Presence.track(socket, socket.assigns.name, %{
+        online_at: inspect(System.system_time(:second))
+      })
+
+    push(socket, "presence_state", Presence.list(socket))
+    {:noreply, socket}
   end
 
   def handle_in("new_msg", %{"body" => body}, socket) do
