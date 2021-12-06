7bd39b777fbb018ad7f79348c506bfb9e1ad885b
Wilmar Stiven Valencia Cardona <wilmar2545@gmail.com>
2021-12-06 16:55:19 -0500

    Add shoppoing cart

--- lib/phoenix_path_web/router.ex
+++ lib/phoenix_path_web/router.ex
@@ -1,5 +1,6 @@
 defmodule PhoenixPathWeb.Router do
   use PhoenixPathWeb, :router
+  alias PhoenixPath.ShoppingCart
 
   pipeline :browser do
     plug :accepts, ["html", "text"]
@@ -9,6 +10,8 @@ defmodule PhoenixPathWeb.Router do
     plug :protect_from_forgery
     plug :put_secure_browser_headers
     plug PhoenixPathWeb.Plugs.Locale, "en"
+    plug :fetch_current_user
+    plug :fetch_current_cart
   end
 
   pipeline :api do
@@ -27,6 +30,11 @@ defmodule PhoenixPathWeb.Router do
     get "/test", PageController, :test
 
     resources "/products", ProductController
+
+    resources "/cart_items", CartItemController, only: [:create, :delete]
+
+    get "/cart", CartController, :show
+    put "/cart", CartController, :update
   end
 
   # Other scopes may use custom stacks.
@@ -59,6 +67,27 @@ defmodule PhoenixPathWeb.Router do
       pipe_through :browser
 
       forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+
+  defp fetch_current_user(conn, _) do
+    if user_uuid = get_session(conn, :current_uuid) do
+      assign(conn, :current_uuid, user_uuid)
+    else
+      new_uuid = Ecto.UUID.generate()
+
+      conn
+      |> assign(:current_uuid, new_uuid)
+      |> put_session(:current_uuid, new_uuid)
+    end
+  end
+
+  def fetch_current_cart(conn, _opts) do
+    if cart = ShoppingCart.get_cart_by_user_uuid(conn.assigns.current_uuid) do
+      assign(conn, :cart, cart)
+    else
+      {:ok, new_cart} = ShoppingCart.create_cart(conn.assigns.current_uuid)
+      assign(conn, :cart, new_cart)
     end
   end
 end
