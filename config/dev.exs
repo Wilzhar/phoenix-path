bc8045db8a16c62dbaea033cc68255a0ac084baa
Wilmar Stiven Valencia Cardona <wilmar2545@gmail.com>
2021-12-07 15:51:37 -0500

    Add custom template to render 404 status code

--- config/dev.exs
+++ config/dev.exs
@@ -21,7 +21,7 @@ config :phoenix_path, PhoenixPathWeb.Endpoint,
   http: [ip: {127, 0, 0, 1}, port: 4000],
   check_origin: false,
   code_reloader: true,
-  debug_errors: true,
+  debug_errors: false,
   secret_key_base: "y79bONO/QUppFFRDvTsy0YsExkoctsIlfng58fHITNpqI2xbYTTgf/1+NzkH3APt",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
