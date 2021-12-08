8e91089919af4500d12fbfd2039e050f41ff0db3
Wilzhar <46170303+Wilzhar@users.noreply.github.com>
2021-12-08 14:59:39 -0500

    Phoenix framework - Chapter 13: Context (#16)
    * Set up Catalog context
    
    * Implement function to Update product views
    
    * Add category schema in the Catalog context and create a product_categories table
    
    * Implement categories in the form
    
    * Add shoppoing cart
    
    * Add validation of input html component to add the listener if exists
    
    * Add orders context

---
+++ lib/phoenix_path/orders/line_item.ex
@@ -0,0 +1,21 @@
+defmodule PhoenixPath.Orders.LineItem do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "order_line_items" do
+    field :price, :decimal
+    field :quantity, :integer
+
+    belongs_to :order, PhoenixPath.Orders.Order
+    belongs_to :product, PhoenixPath.Catalog.Product
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(line_item, attrs) do
+    line_item
+    |> cast(attrs, [:price, :quantity])
+    |> validate_required([:price, :quantity])
+  end
+end
